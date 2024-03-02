4e0db9a64d7194de7da3fce0860fcb0142b82523
carolynenansamba <nansamba.carolyne@yahoo.com>
2024-03-01 20:29:01 -0600

    lab 4 assignment

---
+++ FppFirstHW/src/inclassExerciseDay5/Assignment4Qtn3.java
@@ -0,0 +1,19 @@
+package inclassExerciseDay5;
+
+public class Assignment4Qtn3 {
+
+        public static int arraySum(int[] arr, int n) {
+            if (n == 0) {
+                return 0;
+            } else {
+                return arr[n - 1] + arraySum(arr, n - 1);
+            }
+        }
+
+        public static void main(String[] args) {
+            int[] arr = {1, 2, 3, 4, 5};
+            System.out.println(arraySum(arr, arr.length));
+        }
+    }
+
+
