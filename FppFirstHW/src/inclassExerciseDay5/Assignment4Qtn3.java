e26417c44ad87a8de7e5944e449472033790d63f
carolynenansamba <nansamba.carolyne@yahoo.com>
2024-03-01 20:34:51 -0600

    lab 4 assignment

--- FppFirstHW/src/inclassExerciseDay5/Assignment4Qtn3.java
+++ FppFirstHW/src/inclassExerciseDay5/Assignment4Qtn3.java
@@ -1,6 +1,10 @@
 package inclassExerciseDay5;
 
 public class Assignment4Qtn3 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(arraySum(arr, arr.length));
+    }
 
         public static int arraySum(int[] arr, int n) {
             if (n == 0) {
@@ -10,10 +14,7 @@ public class Assignment4Qtn3 {
             }
         }
 
-        public static void main(String[] args) {
-            int[] arr = {1, 2, 3, 4, 5};
-            System.out.println(arraySum(arr, arr.length));
-        }
+
     }
 
 
