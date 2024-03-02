bb198d3272cb10b2bcf241fed1dd4e62298e42f2
carolynenansamba <nansamba.carolyne@yahoo.com>
2024-03-01 20:28:34 -0600

    lab 4 assignment

---
+++ FppFirstHW/src/inclassExerciseDay5/Assignment4qtn2.java
@@ -0,0 +1,18 @@
+package inclassExerciseDay5;
+
+public class Assignment4qtn2 {
+
+        public static int countDigits(int n){
+        if (n < 10) {
+            return 1;
+        } else {
+            return 1 + countDigits(n / 10);
+        }
+    }
+
+        public static void main (String[]args){
+        System.out.println(countDigits(12345));
+    }
+    }
+
+
