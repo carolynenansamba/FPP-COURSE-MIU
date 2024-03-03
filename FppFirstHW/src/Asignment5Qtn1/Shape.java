24f2c574e077d883bda5a1b4388e305ab5862c46
carolynenansamba <nansamba.carolyne@yahoo.com>
2024-03-02 19:27:07 -0600

    lab 5 assignment

---
+++ FppFirstHW/src/Asignment5Qtn1/Shape.java
@@ -0,0 +1,16 @@
+package Asignment5Qtn1;
+
+public class Shape {
+    String  color;
+
+     Shape(String color){
+        this.color = color;
+    }
+
+    double calculateArea(){
+        return 0.0;
+    }
+    double calculatePerimeter(){
+        return 0.0;
+    }
+}
